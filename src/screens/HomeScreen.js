90cfc9e052b7ec4cab39f069283b00d4e9b4600d
AntonBrito <tonybrito85@gmail.com>
2018-07-13 16:37:33 -0400

    Setup the splash screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+
+import Card from '../components/Card';
 
 class Homecreen extends Component {
     static navigationOption = {
         title : 'Homecreen',
     }
     render() {
+
+        const styles = {
+            mainContainer : {
+                flex: 1,
+                backgroundColor: '#00c0ff',
+            }
+        }
        return(
-        <View>
-            <Text>
-                HomeScreen
-            </Text>
-        </View>
+            <View style={styles.mainContainer}>
+                <Card 
+                title="Beaches"
+                height={550}
+                bgColor='#FFFFFF'/>
+
+                <Button
+                    className="btn"
+                    title="Search Beach"
+                    onPress={() => {
+                        this.props.navigation.navigate('Beaches')
+                    }}
+                >
+                </Button>
+            
+                <Text>
+                    HomeScreen
+                </Text>
+            </View>
        )
    }
 };
