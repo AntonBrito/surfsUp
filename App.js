06a71e91be23b06820b07110c630340b4de4c8ec
AntonBrito <tonybrito85@gmail.com>
2018-07-13 16:37:58 -0400

    Cleanup some code

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 import { createStackNavigator, } from 'react-navigation';
-import LoadingScreen from './src/screens/LoadingScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import BeachScreen from './src/screens/BeachScreen';
 
 
 const App = createStackNavigator({
-  'Loading': { screen: LoadingScreen },
   'Home': { screen: HomeScreen },
   'Beachscreen': { screen: BeachScreen },
 });
