010ebf83c2185ddfec48e758b336e9c1c3fdf99d
AntonBrito <tonybrito85@gmail.com>
2018-07-17 11:10:27 -0400

    Using Picker for dropmenu

--- src/screens/BeachScreen.js
+++ src/screens/BeachScreen.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+
+import Card from '../components/Card';
 
 class BeachScreen extends Component {
+    static navigationOption = {
+        title : ' Beachscreen',
+    }
     render() {
+
+        const styles = {
+            mainContainer : {
+                flex: 1,
+                backgroundColor: '#00c0ff',
+            }
+        }
        return(
-        <View>
+        <View style={styles.mainContainer}>
             <Text>
-                BeachScreen
+                BeachDetail
             </Text>
+
+             <Card 
+                title="BeacheName"
+                height={20}
+                bgColor='#FFFFFF'
+            />
+
+             <Card 
+                title="Swell"
+                height={100}
+                bgColor='#FFFFFF'
+            />
+
+             <Card 
+                title="Weather"
+                height={100}
+                bgColor='#FFFFFF'
+            />
+
+             <Card 
+                title="Wind"
+                height={100}
+                bgColor='#FFFFFF'
+            />
+
+            <Button
+            title="Home"
+            onPress={() => {
+                this.props.navigation.navigate('Home')
+            }}
+            >
+            </Button>
         </View>
        )
    }
